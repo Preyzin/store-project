2098d2bc4e055924e376cc99b4a6ad971b54f6e4
Matheus D. Cadamuro <matheuscadamuro@alunos.utfpr.edu.br>
2020-12-18 22:02:37 -0300

    Implementação de novas funções

--- store_projetc/store_project.c
+++ store_projetc/store_project.c
@@ -3,7 +3,7 @@
 #include "storage.h"
 //Objetivos criar um cadastro de produtos, e fazer o controle de vendas e estoque
 
-int global_ID = 0;
+static int global_ID = 0;
 
 typedef struct produtc{
 
@@ -15,7 +15,116 @@ typedef struct produtc{
 
 }Product;
 
-Product * create(float price, char *name, char *description, int qtt_stock){
+typedef struct cell{
+
+    Product *product;
+    struct Cell *next;
+
+}Cell;
+
+Cell *cell_create(Product *product){
+
+    Cell *cell = malloc(sizeof(Cell));
+
+    cell->product = product;
+
+    cell->next = NULL;
+
+    return cell;
+
+}
+
+
+typedef struct linked_list{
+
+    Cell *head;
+
+}LinkedList;
+
+LinkedList *linked_list_create(){
+
+    LinkedList *linked_list = malloc(sizeof(LinkedList));
+
+    linked_list->head = NULL;
+
+    return linked_list;
+}
+
+static int verify_empty_linked_list(LinkedList *linked_list){
+
+    return (linked_list == NULL) || (linked_list->head == NULL);
+
+}
+
+void linked_list_insert_first(Product *product, LinkedList *linked_list){
+
+    Cell *aux;
+
+    if (linked_list == NULL)
+        linked_list = linked_list_create();
+
+    aux = cell_create(product);
+
+    aux->next = linked_list->head;
+
+    linked_list->head = aux;
+}
+
+void linked_list_insert_last(Product *product, LinkedList *linked_list){
+    Cell *aux, *new_cell;
+
+    if (linked_list == NULL)
+        linked_list = linked_list_create();
+
+    if (verify_empty_linked_list(linked_list))
+        linked_list_insert_first(product, linked_list);
+    else{
+        aux = linked_list->head;
+
+        while(aux->next != NULL)
+            aux = aux->next;
+
+        new_cell = cell_create(product);
+
+        aux->next = new_cell;
+    }
+}
+
+void linked_list_insert(Product *product, LinkedList *linked_list){
+
+    Cell *aux;
+
+    if(linked_list == NULL)
+        linked_list = linked_list_create();
+
+    if(verify_empty_linked_list(linked_list))
+        linked_list_insert_first(product, linked_list);
+    else
+        linked_list_insert_last(product, linked_list);
+
+}
+
+void print_linked_list(LinkedList *linked_list){
+
+    system("cls || clear");
+
+    Cell *aux;
+
+    if(!verify_empty_linked_list(linked_list)){
+        aux = linked_list->head;
+
+        while(aux != NULL){
+            product_print(aux->product);
+            aux = aux->next;
+        }
+
+    }
+
+    system("pause");
+
+}
+
+Product * product_create(float price, char *name, char *description, int qtt_stock){
 
     Product *product = (Product*) malloc(sizeof(Product));
 
@@ -48,7 +157,7 @@ void product_print(Product *product){
         printf("Name: %s\n", product->name);
         printf("Description: %s\n", product->description);
         printf("Price: $ %.2f\n", product->price);
-        printf("Quantity in stock: %d\n", product->qtt_stock);
+        printf("Quantity in stock: %d (un)\n", product->qtt_stock);
     }
     else{
         printf("\nPrint ERROR product doesnt exists\n");
@@ -56,9 +165,41 @@ void product_print(Product *product){
 
 }
 
+void product_register_menu(LinkedList *product_linked_list){
+
+    char product_name[120], product_description[250];
+    float product_price;
+    int product_qtt_stock;
+
+    system("cls || clear");
+
+    printf("Product register menu\n\n");
+
+
+    fflush(stdin);
+    printf("Product Name: ");
+    gets(product_name);
+
+    fflush(stdin);
+    printf("\nProduct Description: ");
+    gets(product_description);
+
+    printf("\nProduct Price: $ ");
+    scanf("%f", &product_price);
+
+    printf("\nQuantity in Stock (un): ");
+    scanf("%d", &product_qtt_stock);
+
+    linked_list_insert(product_create(product_price, product_name, product_description, product_qtt_stock), product_linked_list);
+
+}
+
+
 void menu(){
 
     int conf;
+
+    LinkedList *product_linked_list = linked_list_create();
 
     do{
         system("cls || clear");
@@ -72,11 +213,14 @@ void menu(){
 
         switch (conf){
         case 1:
-            system("cls");
+
+            product_register_menu(product_linked_list);
 
             break;
 
         case 2:
+
+            print_linked_list(product_linked_list);
 
             break;
 
--- store_projetc/store_project.h
+++ store_projetc/store_project.h
@@ -1,6 +1,26 @@
 
 typedef struct produtc Product;
 
-Product * create(float price, char *name, char *description, int qtt_stock);
+typedef struct cell Cell;
+
+typedef struct linked_list LinkedList;
+
+LinkedList *linked_list_create();
+
+static int verify_empty_linked_list(LinkedList *linked_list);
+
+void linked_list_insert_first(Product *product, LinkedList *linked_list);
+
+void linked_list_insert_last(Product *product, LinkedList *linked_list);
+
+void linked_list_insert(Product *product, LinkedList *linked_list);
+
+void print_linked_list(LinkedList *linked_list);
+
+Product * product_create(float price, char *name, char *description, int qtt_stock);
 
 void product_print(Product *product);
+
+void product_register_menu(LinkedList *product_linked_list);
+
+void menu();
